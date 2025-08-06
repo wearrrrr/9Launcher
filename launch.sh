1516025df205bdd2c1e36704deb38da1793d4435
wearrrrr <contact@wearr.dev>
2025-08-06 13:17:05 -0500

    downgrade ubuntu container to 22.04 (please work please work please work)

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./build.sh
-if ([ -f bin/NineLauncher ]); then
-    ./bin/NineLauncher
+if ([ -f bin/9Launcher ]); then
+    ./bin/9Launcher
     else
     printf "\x1b[1;31mNineLauncher not found, build likely failed!\n"
 fi
